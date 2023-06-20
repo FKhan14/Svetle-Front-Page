cc4a3f570a38b1c98d9bb8690d0de87357cb9a28
Irw25-lschs <ianwhitaker2024@lschs.org>
2023-06-20 14:03:39 -0400

    removed buttons

--- new-world/src/App.svelte
+++ new-world/src/App.svelte
@@ -1,12 +1,6 @@
 <script>
 	import data from './demo-output.json';
-	function addRow() {
-		data = [...data, [...newRow]]
-		newRow = columns
-	}
-	function deleteRow(rowToBeDeleted) {
-		data = data.filter(row => row != rowToBeDeleted)
-	} 
+	
 	console.log(data);
 	let columns = ["VMName", "IP", "HostName", "Status", "LastCheckInTime", "HyperVisor"]
 	let newRow = [...columns];
@@ -28,14 +22,14 @@
 			{#each row as cell}
 			<td contenteditable="true" bind:innerHTML={cell} />
 			{/each}
-			<button on:click={(data) => deleteRow(row)}>X</button>
+		
 		</tr>
 	{/each}
 	<tr style="color: grey">
 		{#each newRow as column}
 			<td contenteditable="true" bind:innerHTML={column} />
 		{/each}
-		<button on:click={addRow}>add</button>
+		
 	</tr>
 	<pre style="background: #eee">{JSON.stringify(data, null, 2)}</pre> 
 </table>
